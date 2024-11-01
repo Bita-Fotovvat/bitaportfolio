93df51c50cbf60360e66818c5091d15dc699a954
Bita Fotovvat <bita.ftvt@gmail.com>
2024-11-01 17:37:26 -0400

    added the active state functionality of about list items

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,21 +1,32 @@
 import './Navbar.scss';
 import {useState} from 'react';
 export default function Navbar(){
-    const [activeNavItem, setActiveNavItem] = useState(false);
+    const [activeNavItem, setActiveNavItem] = useState(null);
     const navItems = [
-        {id:1, value:"About"}, 
-        {id:2, value:"Experience"}, 
+        {id:1, value:"About"},
+        {id:2, value:"Experience"},
         {id:3, value:"Projects"}
     ];
-    function handleClick(e){
-        setActiveNavItem(!activeNavItem);
+    function handleClick(index){
+        setActiveNavItem(index);
     }
 
     return(
         <>
         <ul className="navbar">
             {navItems.map((item)=>{
-                return(<li key={item.id}>{item.value}</li>)
+                return(
+                <section className='navbar--item__container'>
+                    <div className={`navbar--item__line ${activeNavItem === item.id ? "active--line" : ""}`} ></div>
+                    <li 
+                        key={item.id} 
+                        className={`navbar--item__title ${activeNavItem === item.id ? "active" : ""}`} 
+                        onClick={()=>{handleClick(item.id)}}
+                        >
+                        {item.value}
+                    </li>
+                </section>
+  )
             })}
         </ul>
         </>
