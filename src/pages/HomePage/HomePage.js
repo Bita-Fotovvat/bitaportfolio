494392c23ec4778b5cf5d81b7eb047b6f2ef98fa
Bita Fotovvat <bita.ftvt@gmail.com>
2024-11-01 16:53:29 -0400

    added Intro styling

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,4 +1,3 @@
-import Header from '../../components/Header/Header';
 import Intro from '../../components/Intro/Intro';
 import Navbar from '../../components/Navbar/Navbar';
 import Connect from '../../components/Connect/Connect';
@@ -6,10 +5,14 @@ import Connect from '../../components/Connect/Connect';
 export default function HomePage() {
     return (
       <div className="home">
-        <Header/>
-        <Intro/>
-        <Navbar/>
-        <Connect/>
+        <section className="home--leftbar">
+          <Intro/>
+          <Navbar/>
+          <Connect/>
+        </section>
+        <section className="home--rightbar">
+          
+        </section>
       </div>
     );
   }
