4eabcb1cf0655aa93f9c63492009e60c118aa8bc
Bita Fotovvat <bita.ftvt@gmail.com>
2024-11-06 18:54:34 -0500

    styling added

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -22,6 +22,13 @@ export default function HomePage() {
       body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
     }
   ];
+    // const projectDetails = [
+    //   {title: "Travel Planner",
+    //     description: "",
+    //     image: ""
+    //   },
+    //   {}
+    // ];
   const about = propsData[0];
   const projects = propsData[1];
   const experience = propsData[2];
