98346897893c1648530e791254ae089d75975423
Bita Fotovvat <bita.ftvt@gmail.com>
2024-11-06 17:01:33 -0500

    added details to contact component

--- src/components/Connect/Connect.js
+++ src/components/Connect/Connect.js
@@ -1,11 +1,15 @@
+import './Connect.scss';
+import GitHubIcon from '../../assets/icons/github-mark.png';
+import LinkedinIcon from '../../assets/icons/linkedin.png';
+
 export default function Connect(){
     return(
         <>
-        <ul>
-            <li>Github</li>
-            <li>Linkedin</li>
-            <li>Email</li>
-        </ul>
+        <div className='connect--list'>
+            <li className='connect--list__item'><image className='connect--img' src={GitHubIcon} alt="Github Icon"/></li>
+            <li className='connect--list__item'><image className='connect--img' src={LinkedinIcon} alt="Linkedin Icon"/></li>
+            <li className='connect--list__item'>Email</li>
+        </div>
         </>
     );
 }
